bf59e55d11e9d0ac11705101d4e5ad459e36fb5a
AAAlexey <alex@sundiger.ru>
2021-10-08 14:24:22 +0700

    first commit

---
+++ precache-manifest.44aabd85646551f0d55a43ce20fabc9f.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a074abdf096f8a348c666409ddbf1d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc6e4f41636f65f4321c",
+    "url": "/static/css/main.974d5ddb.chunk.css"
+  },
+  {
+    "revision": "9419d7bfa1c7c0e3b108",
+    "url": "/static/js/2.db271f52.chunk.js"
+  },
+  {
+    "revision": "fc6e4f41636f65f4321c",
+    "url": "/static/js/main.3ae243c3.chunk.js"
+  },
+  {
+    "revision": "e73f5af40768818fd4cd",
+    "url": "/static/js/runtime~main.060fb489.js"
+  },
+  {
+    "revision": "effc7a8ce55837facb05aee9e6bd7a7f",
+    "url": "/static/media/01d.effc7a8c.svg"
+  },
+  {
+    "revision": "effc7a8ce55837facb05aee9e6bd7a7f",
+    "url": "/static/media/01n.effc7a8c.svg"
+  },
+  {
+    "revision": "d4a8aa5580bd13837fa6dc88a7401882",
+    "url": "/static/media/02d.d4a8aa55.svg"
+  },
+  {
+    "revision": "d4a8aa5580bd13837fa6dc88a7401882",
+    "url": "/static/media/02n.d4a8aa55.svg"
+  },
+  {
+    "revision": "960b324df385a7aa9f5e3ba2d2c5c2f4",
+    "url": "/static/media/03d.960b324d.svg"
+  },
+  {
+    "revision": "960b324df385a7aa9f5e3ba2d2c5c2f4",
+    "url": "/static/media/03n.960b324d.svg"
+  },
+  {
+    "revision": "7eedcf5fc4568e7f02f9970b2310a662",
+    "url": "/static/media/04d.7eedcf5f.svg"
+  },
+  {
+    "revision": "7eedcf5fc4568e7f02f9970b2310a662",
+    "url": "/static/media/04n.7eedcf5f.svg"
+  },
+  {
+    "revision": "efc5a4e3f8f84a766b9b393d11b56050",
+    "url": "/static/media/09d.efc5a4e3.svg"
+  },
+  {
+    "revision": "efc5a4e3f8f84a766b9b393d11b56050",
+    "url": "/static/media/09n.efc5a4e3.svg"
+  },
+  {
+    "revision": "d98fe1e499ac3d5bf0cbaa5614fcf842",
+    "url": "/static/media/10d.d98fe1e4.svg"
+  },
+  {
+    "revision": "d98fe1e499ac3d5bf0cbaa5614fcf842",
+    "url": "/static/media/10n.d98fe1e4.svg"
+  },
+  {
+    "revision": "3c76c82e000d92ff9dcb703e04beb11d",
+    "url": "/static/media/11d.3c76c82e.svg"
+  },
+  {
+    "revision": "3c76c82e000d92ff9dcb703e04beb11d",
+    "url": "/static/media/11n.3c76c82e.svg"
+  },
+  {
+    "revision": "b8d8ad4b2d254295a1853f15e503df11",
+    "url": "/static/media/13d.b8d8ad4b.svg"
+  },
+  {
+    "revision": "b8d8ad4b2d254295a1853f15e503df11",
+    "url": "/static/media/13n.b8d8ad4b.svg"
+  },
+  {
+    "revision": "1186186b80a5955ab64b11ce34c975cf",
+    "url": "/static/media/50d.1186186b.svg"
+  },
+  {
+    "revision": "1186186b80a5955ab64b11ce34c975cf",
+    "url": "/static/media/50n.1186186b.svg"
+  },
+  {
+    "revision": "2247af05d71c87eda56230f4cf63f230",
+    "url": "/static/media/background2.2247af05.jpg"
+  },
+  {
+    "revision": "222a8c32a649a55c3f7f8fa738e48768",
+    "url": "/static/media/cloudy_moon.222a8c32.svg"
+  }
+]);
